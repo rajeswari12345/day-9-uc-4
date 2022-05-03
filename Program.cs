f06a42dae848b5ddb49d291985cd6dee402b12a0
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-03 12:24:03 +0530

    day 9 uc 4

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+class program
+{
+    static void Main(string[] args)
+    {
+
+        Person person = new Person();
+        Console.Write("Enter your Name: ");
+        person.Name = Console.ReadLine();
+
+        Console.Write("name is mani ");
+        person.name = Console.ReadLine();
+
+        Console.Write("delete you name: ");
+        person.Name = Console.ReadLine();
+    }
+}
+
+
+
+
